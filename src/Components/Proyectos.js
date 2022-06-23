6f41dafcb10dc80ed9afa0577c2db8b52c113cb6
Franco Velasco <francovelasco10@hotmail.com>
2022-06-23 12:20:28 -0500

    Adicion Proyecto

--- src/Components/Proyectos.js
+++ src/Components/Proyectos.js
@@ -77,14 +77,15 @@ const Proyectos = () => {
               src="https://res.cloudinary.com/aca-geek/image/upload/v1655756956/PORTAFOLIO/VisualizadorGithub_qaktcn.png"
               className="Image"
               width="280px"
-              height='100px'
+              height="100px"
               alt=""
             />
             <div className="Info">
               <h1 style={{ fontWeight: "500" }}>Github Info</h1>
-              <h2 style={{marginBottom:'4.5rem'}}>
+              <h2 style={{ marginBottom: "4.5rem" }}>
                 Aqui podras encontrar a Cualquier persona dentro de Github
-                Permitiendote añadirla a la lista, Editar su usuario Y Eliminarlo
+                Permitiendote añadirla a la lista, Editar su usuario Y
+                Eliminarlo
               </h2>
               <div className="Tech">
                 <h2>
@@ -121,6 +122,66 @@ const Proyectos = () => {
                 <a
                   style={{ color: "#cdcdff" }}
                   href="https://prueba-tecnica-aca-geek.vercel.app/RegistroData1"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <ImShare size={23} style={{}} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="Proyecto">
+          <div style={{ pointerEvents: "none" }}>
+            <img
+              src="https://res.cloudinary.com/aca-geek/image/upload/v1656004763/PORTAFOLIO/Peliculas_ghm9cf.png"
+              className="Image"
+              width="280px"
+              height="100px"
+              alt=""
+            />
+            <div className="Info">
+              <h1 style={{ fontWeight: "500" }}>Peliculas</h1>
+              <h2 style={{ marginBottom: "4.5rem" }}>
+                Podras ver informacion acerca de las ultimas peliculas
+                publicadas, sus calificaciones, y mucho mas!!
+              </h2>
+              <div className="Tech">
+                <h2>
+                  {" "}
+                  <SiJavascript
+                    size={23}
+                    style={{
+                      marginBottom: ".8rem",
+                      marginTop: ".1rem",
+                    }}
+                  />
+                </h2>
+                <h2>
+                  <SiReact size={22} style={{}} />
+                </h2>
+                <h2>
+                  <SiChakraui
+                    size={25}
+                    style={{
+                      marginBottom: ".8rem",
+                    }}
+                  />
+                </h2>
+              </div>
+              <div className="LinkProyectos">
+                <a
+                  style={{ color: "#cdcdff" }}
+                  href="https://github.com/Francovo/Sprint3Grupo7"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <SiGithub size={23} />
+                </a>
+                <a
+                  style={{ color: "#cdcdff" }}
+                  href="https://sprint3-grupo7.vercel.app/home"
                   target="_blank"
                   rel="noreferrer"
                 >
