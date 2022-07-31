a974f2d7a6dd70257e56cbc0d9dd9e8473b8ace5
Franco Velasco <francovelasco10@hotmail.com>
2022-07-31 13:00:14 -0500

    Actualizacion Avatar

--- src/Components/Biografia.js
+++ src/Components/Biografia.js
@@ -1,50 +1,36 @@
-import React from "react";
-import "../styles/Biografia.scss";
-import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import React from 'react';
+import '../styles/Biografia.scss';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 const Biografia = () => {
-  return (
-    <>
-      <div className="ContainerAll">
-        <div className="Biografia" id="Biografia">
-          <img
-            className="Avatar"
-            alt=""
-            src="https://yt3.ggpht.com/rFWOScFEeaaXTKW9WudfrgZ-GTaI7lQ9UPOg17U7xz9lciOjgHWyBinDF95yHywj-SK7ko1GeMk=s176-c-k-c0x00ffffff-no-rj-mo"
-          />
-          <h1>Hola, Soy Franco Velasco</h1>
-          <h2>Front End Developer</h2>
-          <div className="About_Me">
-            <h1 className="ME">
-              Bienvenido a mi portafolio, hecha un vistazo y conoce acerca de
-              mi. Quien sabe, talvez podamos trabajar juntos.
-              <br />
-              Igualmente, me llamo Franco y soy desarrollador web, soy muy
-              proactivo y siempre quiero ser el mejor en lo que hago. <br />
-              <br />
-              Si estas de acuerdo con eso, nos llevaremos muy bien.
-            </h1>
-            <div className="Redes">
-              <a
-                href="https://www.linkedin.com/in/franvov/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillLinkedin size={30} />
-              </a>
-              <a
-                href="https://github.com/Francovo"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillGithub size={30} />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="ContainerAll">
+				<div className="Biografia" id="Biografia">
+					<img className="Avatar" alt="" src="https://png.pngtree.com/png-clipart/20190115/ourlarge/pngtree-programmer-cartoon-hand-painted-table-lamp-png-image_348941.jpg" />
+					<h1>Hola, Soy Franco Velasco</h1>
+					<h2>Front End Developer</h2>
+					<div className="About_Me">
+						<h1 className="ME">
+							Bienvenido a mi portafolio, hecha un vistazo y conoce acerca de mi. Quien sabe, talvez podamos trabajar juntos.
+							<br />
+							Igualmente, me llamo Franco y soy desarrollador web, soy muy proactivo y siempre quiero ser el mejor en lo que hago. <br />
+							<br />
+							Si estas de acuerdo con eso, nos llevaremos muy bien.
+						</h1>
+						<div className="Redes">
+							<a href="https://www.linkedin.com/in/franvov/" target="_blank" rel="noreferrer">
+								<AiFillLinkedin size={30} />
+							</a>
+							<a href="https://github.com/Francovo" target="_blank" rel="noreferrer">
+								<AiFillGithub size={30} />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Biografia;
